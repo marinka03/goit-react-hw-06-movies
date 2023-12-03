7858d1924d6b65e12313a50e7894325f7be109e3
marinka03 <97391927+marinka03@users.noreply.github.com>
2023-12-03 21:59:34 +0000

    Deploying to gh-pages from @ marinka03/goit-react-hw-05-movies@6ee71592316c4b8ae7d9bd24bb303c1737c88fbb 🚀

---
+++ static/js/71.a024c523.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[71],{71:function(t,e,n){n.d(e,{gmZ:function(){return o}});var r=n(9983);function o(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinejoin:"round",strokeWidth:"32",d:"M240 424v-96c116.4 0 159.39 33.76 208 96 0-119.23-39.57-240-208-240V88L64 256z"}}]})(t)}}}]);
+//# sourceMappingURL=71.a024c523.chunk.js.map
